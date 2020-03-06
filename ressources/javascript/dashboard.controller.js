de4f1cb5bd0c27a5d3fef814fce8b45ad348f945
Stephen CHEVALIER <contact@nextwab.com>
2020-03-06 04:02:34 +0100

    v1.0017 - Add MLP Chat

--- ressources/javascript/dashboard.controller.js
+++ ressources/javascript/dashboard.controller.js
@@ -51,6 +51,12 @@ Dashboard.controller('DashboardController', function($rootScope , $scope, UserSe
     
 	dashboard.Init                      = function() {
         dashboard.UpdateInterface();
+        
+        MLP_UserName = dashboard.ServiceUser.Login_Mail+' '+dashboard.ServiceUser.Login_HashKey;
+        MLP_BusyText = 'Techniciens actuellement indisponibles pour un chat live. <a href="https://dashboard.nextwab.net/support/">Envoyer un ticket ?</a>';
+        MLP_OnlineClass = 'blink';
+        
+        $('body').append($("<script>", {src: "https://my-love.page/api-core/chat-engine/Vs7jZNcI8FRt?CSS_File=https%3A%2F%2Fwww.nextwab.com%2Fstyle%2Fmy-love-page.css"})); 
         };
         
         
@@ -64,6 +70,8 @@ Dashboard.controller('DashboardController', function($rootScope , $scope, UserSe
         dashboard.pageViewConfig    = pageViewConfig ; 
 		dashboard.pageHandler       = '/'+dashboard.pageViewBase+'/_commons/_'+pageViewConfig.handler+'.html';
 		dashboard.pageRequested     = '/'+dashboard.pageViewBase+pageViewConfig.file;
+        
+        MLP_UserName = dashboard.ServiceUser.Login_Mail+' '+dashboard.ServiceUser.Login_HashKey;
         }
 	});
 
