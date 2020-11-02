caebe8e6b2e1723cc150e63c57360d1d8919644a
Stephen CHEVALIER <contact@nextwab.com>
2020-11-02 03:15:58 +0100

    v1.0020.5 - Fix config file

--- config/config.js
+++ config/config.js
@@ -1,9 +1,7 @@
-Dashboard.value('config', 
-	{
-	applicationURL 	        : "https://dashboard.nextwab.net",
-	applicationURL_Chat		: "https://chat.netwab.net",
+Dashboard.value('config', {
+	applicationURL				: "https://dashboard.nextwab.net",
+	applicationURL_Chat			: "https://chat.nextwab.net",
     
-    applicationHostname         : "dashboard.nextwab.net",
-    applicationHostname_Chat    : "chat.netwab.net"
-	}
-);
+	applicationHostname			: "dashboard.nextwab.net",
+	applicationHostname_Chat	: "chat.nextwab.net"
+});
