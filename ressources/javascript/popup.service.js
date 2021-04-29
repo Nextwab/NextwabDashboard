a7858b1185604aa53acc4c15f8af5f4f765150eb
Stephen CHEVALIER <contact@nextwab.com>
2021-04-29 23:14:44 +0200

    Fix for Google Chrome 90

--- ressources/javascript/popup.service.js
+++ ressources/javascript/popup.service.js
@@ -5,27 +5,27 @@ Dashboard.service("PopupService", function($compile , $location){
 	var vm_Popup = this;
 	vm_Popup.dashboard = null; 
 	vm_Popup.Instances = [];
-	
+
 	// Init dashboard
 	vm_Popup.setDashboard = function(dashboard){
 		vm_Popup.dashboard = dashboard;
 	};
-    
+
 	// Open new modal windows
 	vm_Popup.openNew = function( Config ){
-		angular.element(document.body).append($compile('<popup ng-style=\''+JSON.stringify(Config.Styles)+'\' class="'+Config.ClassName+'" config=\''+JSON.stringify(Config)+'\'></popup>')(vm_Popup.dashboard.scope));
+		angular.element(document.body).append($compile('<popup-custom ng-style=\''+JSON.stringify(Config.Styles)+'\' class="'+Config.ClassName+'" config=\''+JSON.stringify(Config)+'\'></popup-custom>')(vm_Popup.dashboard.scope));
         $('.background_overlay').fadeIn();
 	};
-    
+
     // Close
 	vm_Popup.close = function(Popup) {
-        $('popup').fadeOut().remove(); 
+        $('popup-custom').fadeOut().remove(); 
         $('.background_overlay').fadeOut();
-    }
+    };
 });
 
-Dashboard.directive("popup", function(NavigationService){
-	
+Dashboard.directive("popupCustom", function(NavigationService){
+
 	var PopupConfig = {
 		restrict        : 'EA',
 		replace         : false,
@@ -38,63 +38,55 @@ Dashboard.directive("popup", function(NavigationService){
 		link            : function(scope, element, attrs){
 		}
 	};
-	
+
 	return PopupConfig;
     });
 
-    
-    
 // Popup Controller injected in directive 
 var PopupController = function($scope, $element, $window, $document, NavigationService){
-			
+
 	var vm_Popup                            = this;
-	
+
     vm_Popup.POPUP_Endpoint_URL             = "_not_found";
     vm_Popup.POPUP_Title                    = "";
     vm_Popup.Drag                           = false;
     vm_Popup.Styles                         = {};
     vm_Popup.Config                         = {};
-    
-	if(typeof $element[0].attributes.config.nodeValue !== "undefined")
-		{
+	if(typeof $element[0].attributes.config.nodeValue !== "undefined") {
 		var config                          = JSON.parse($element[0].attributes.config.nodeValue);
 		
         vm_Popup.Config                     = config;
         vm_Popup.Styles                     = config.Styles;
         vm_Popup.POPUP_Endpoint_URL         = NavigationService.getEndpointURL(config.Endpoint, config.Action );
         vm_Popup.POPUP_Title                = config.Title;
-		}
-    
-    
+	}
+
 	vm_Popup.POPUP_PageToInclude            =  '/pages'+NavigationService.getViewConfig(vm_Popup.POPUP_Endpoint_URL).file;
-    
-    
+
     vm_Popup.Close  = function(Popup) {
         $($element).fadeOut().remove(); 
         
-        if($('popup').length == 0) {
+        if($('popup-custom').length == 0) {
             $('.background_overlay').fadeOut();
         }
-    }
-    
-    
+    };
+
     // Drag popup - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     $element.find('.Popup_Header').bind("mousedown", function(e) {
         vm_Popup.Drag                       = {origin_x : e.pageX , origin_y : e.pageY, X:  angular.element($element).prop('offsetLeft'), Y:  angular.element($element).prop('offsetTop') };
         return false;
     });
-    
+
     $element.find('.Popup_Header').bind("mouseup", function(e) {
         vm_Popup.Drag                       = false;
         return false;
     });
 
     $document.mousemove(function(e) {
-
         if(vm_Popup.Drag){
             var left = (vm_Popup.Drag.X - (vm_Popup.Drag.origin_x - e.pageX) );
             var top = (vm_Popup.Drag.Y - (vm_Popup.Drag.origin_y - e.pageY) );
-           
+
             $($element).css('left' , left +'px');
             $($element).css('top' , top +'px');
             $($element).css('right' , 'auto');
@@ -102,7 +94,6 @@ var PopupController = function($scope, $element, $window, $document, NavigationS
 
     });
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-    
 };
 
 
